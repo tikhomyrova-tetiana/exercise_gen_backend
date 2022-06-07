104978b68343283328639e56637d5d6a683328dd
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-06-07 17:30:07 +0200

    added_auth_in_favourites

--- routers/exercises.js
+++ routers/exercises.js
@@ -25,6 +25,7 @@ router.get("/equipment/:equipment", async (req, res) => {
 });
 
 // Create a new exercise
+// http POST :4000/exercises/favourites apiId=3201 Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1NDYwMzA4NSwiZXhwIjoxNjU0NjEwMjg1fQ.UnQi0wnvUexY1xC3BAh_OpbyzJsgIXCd2bxKJrpg4M0"
 router.post("/favourites", authMiddleware, async (req, res) => {
   try {
     const { apiId } = req.body;
@@ -52,37 +53,46 @@ router.post("/favourites", authMiddleware, async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  /* body = {
-        userId: req.user USING MIDDLE WARE
-        exerciseId: exercise.id from API
-    } */
 });
 
-router.get("/favourites", async (req, res, next) => {
+router.get("/favourites", authMiddleware, async (req, res, next) => {
+  // Add authmiddleware to get user id and then find only favorites of this user
+  const userId = req.user.id;
   try {
-    const allFavourites = await Favourites.findAll();
+    const allFavourites = await Favourites.findAll({
+      where: { userId: userId },
+      include: [Exercises],
+    });
     res.send(allFavourites);
   } catch (error) {
     console.log(error.message);
   }
 });
 
-// GET all exercises - don't need it for now
-router.get("/", async (req, res) => {
-  const options = {
-    method: "GET",
-    url: "https://exercisedb.p.rapidapi.com/exercises",
-    headers: {
-      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      "X-RapidAPI-Key": "d067087cb5msh916566a37810f52p163719jsnf04db4e1577b",
-    },
-  };
-  try {
-    const { data } = await axios.request(options);
-    res.status(200).send(data);
-  } catch (err) {
-    console.log("error".err);
-  }
-});
+// router.get("/", async (req, res, next) => {
+//   try {
+
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+// });
+
+// GET all exercises from external API - don't need it for now
+// router.get("/", async (req, res) => {
+//   const options = {
+//     method: "GET",
+//     url: "https://exercisedb.p.rapidapi.com/exercises",
+//     headers: {
+//       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+//       "X-RapidAPI-Key": "d067087cb5msh916566a37810f52p163719jsnf04db4e1577b",
+//     },
+//   };
+//   try {
+//     const { data } = await axios.request(options);
+//     res.status(200).send(data);
+//   } catch (err) {
+//     console.log("error".err);
+//   }
+// });
 
 module.exports = router;
